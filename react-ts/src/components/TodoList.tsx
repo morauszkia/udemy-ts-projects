3172053416e543042b35ea67bdc96dc41d8aca13
András Morauszki <morauszki.andras@gmail.com>
2022-12-27 21:40:28 +0100

    react-ts: finished project

--- react-ts/src/components/TodoList.tsx
+++ react-ts/src/components/TodoList.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 
+import './TodoList.css';
+
 interface TodoListProps {
   items: { id: string; text: string }[];
+  onDeleteTodo: (id: string) => void;
 }
 
-const TodoList: React.FC<TodoListProps> = ({ items }) => {
+const TodoList: React.FC<TodoListProps> = ({ items, onDeleteTodo }) => {
   return (
     <ul>
       {items.map((todo) => (
-        <li key={todo.id}>{todo.text}</li>
+        <li key={todo.id}>
+          <span>{todo.text}</span>
+          <button onClick={onDeleteTodo.bind(null, todo.id)}>Delete</button>
+        </li>
       ))}
     </ul>
   );
